824ade253d6709629d0976888b5b96a97ec3eb86
sgonzalo <santiago.gonzalo360@gmail.com>
2019-10-29 21:46:01 +0000

    push

--- src/js/index.js
+++ src/js/index.js
@@ -9,14 +9,24 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let newName = "Jeff";
+  if (variables.name !== null) newName = variables.name;
+  let newLastname = "Lebowski";
+  if (variables.lastname !== null) newLastname = variables.lastname;
+  let newRole = "Unemployed";
+  if (variables.role !== null) newRole = variables.role;
+  let newCity = "Malibu";
+  if (variables.city !== null) newCity = variables.city;
+  let newCountry = "USA";
+  if (variables.country !== null) newCountry = variables.country;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city} ${variables.country}</h3>
+          <h1>${newName} ${newLastname}</h1>
+          <h2>${newRole}</h2>
+          <h3>${newCity} ${newCountry}</h3>
           <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
